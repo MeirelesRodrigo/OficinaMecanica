6f41ea6b78fb865e0e1a6e775e3787c8f822cb35
Rodrigo Meireles <meireles.rsilva22@gmail.com>
2025-03-01 16:25:04 -0300

    integracao com firebase

---
+++ environments/environment.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcHcpddUhXEhLRFTcErb6qQLul59y65KY",
+  authDomain: "oficina-pardal.firebaseapp.com",
+  projectId: "oficina-pardal",
+  storageBucket: "oficina-pardal.firebasestorage.app",
+  messagingSenderId: "1087286976398",
+  appId: "1:1087286976398:web:bdacb853d9123d754fcedf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
